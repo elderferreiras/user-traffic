665e3c4fa54c2c29b13b4be75adc4145cf6b6fc2
Elder Patten Ferreira <elderferreirass@gmail.com>
2020-01-18 15:09:47 -0700

    update: y axis domain in the conversion chart

--- src/components/user/ConversionsChart.js
+++ src/components/user/ConversionsChart.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { LineChart, Line } from 'recharts';
+import { LineChart, Line, YAxis } from 'recharts';
 
 const ConversionsChart = (props) => {
     return (
         <div className="chart-container">
             <LineChart width={150} height={50} data={props.conversionsPerDay.data}>
                 <Line type="monotone" dataKey="conversions" stroke="#000000" strokeWidth={1} dot={false}/>
+                <YAxis type="number" domain={['dataMin', 'dataMax']} hide={true}/>
             </LineChart>
             <div className="chart-footer">
                 Conversions {props.conversionsPerDay.range.from} - {props.conversionsPerDay.range.to}
